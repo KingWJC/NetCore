cd123d0940cd0d68b0919dadebfb878fcad047ea
KingWJC <15110038963@163.com>
2020-05-25 16:58:17 +0800

    modify try-catch-finally

--- NetCore/testcore/testconsole/Program.cs
+++ NetCore/testcore/testconsole/Program.cs
@@ -7,10 +7,15 @@ namespace testconsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            Console.WriteLine(Test());
+            string str = TestTryCatch();
+            Console.WriteLine("\nResult:" + str);
         }
 
+        /**
+         * @description: Try-Catch-Finilly 中return的用法
+         * @param {type} 
+         * @return: string
+         */
         public static string TestTryCatch()
         {
             var stringBuilder = new System.Text.StringBuilder();
@@ -34,37 +39,21 @@ namespace testconsole
             }
             finally
             {
+                /*finally不管什么情况都会执行，包括try catch 里面用了return 的情况。
+                如果try catch 里面用了return，则finally的执行不影响返回结果。
+                 一般用于数据库连接的关闭*/
                 Console.WriteLine("3");
 
                 stringBuilder.Append("\nprint Finally!");
 
+                Console.Write("Finally:" + stringBuilder.ToString());
+
                 //error: 控制不能离开 finally 子句主体
                 //return stringBuilder.ToString();
             }
-            // return stringBuilder.ToString();
-        }
 
-        static int Test()
-        {
-            int num = 0;
-            try
-            {
-                num = 1;
-                throw new Exception("手动控制抛出异常");
-                // return num;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                num++;
-                // return num;
-            }
-            finally
-            {
-                num++;
-                Console.WriteLine("我在Finally中num={0}", num);
-            }
-            return num;
+            //无法访问的代码
+            // return stringBuilder.ToString();
         }
     }
 }
